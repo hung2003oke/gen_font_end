f595c0082fdd4af70064c64a88db6a78ea762506
Phạm Nguyên Hưng <nguyenhung682003@gmail.com>
2024-01-09 11:59:45 +0700

    09/01/2023

--- split_css.py
+++ split_css.py
@@ -1,122 +1,3 @@
-# import re
-#
-# def read_data(filepath):
-#     with open(filepath, encoding="utf-8") as h:
-#         return h.read()
-#
-#
-# class SplitDiv:
-#     def __init__(self):
-#         self.matching_pair_advance = None
-#         self.matching_pairs = None
-#         self.lines = None
-#         self.space_length = None
-#         self.start_css = None
-#         self.css_list = None
-#
-#     def set_lines(self, lines):
-#         self.lines = lines
-#         self.count_space()
-#         self.calculate_start_css()
-#
-#     def count_space(self):
-#         cs = {}
-#         for i, line in enumerate(self.lines):
-#             t = 0
-#             while t < len(line) and line[t].isspace():
-#                 t += 1
-#             cs[i] = t
-#         self.space_length = cs
-#
-#     def calculate_start_css(self):
-#         cards = {}
-#         for i in range(len(self.lines)):
-#             cards[i] = not (self.lines[i].strip().startswith(".") and self.lines[i].strip().endswith("{") or self.lines[
-#                 i].strip().endswith("}"))
-#         self.start_css = cards
-#
-#     def is_end_css(self, start, now) -> bool:
-#         return self.space_length[start] == self.space_length[now] and self.lines[now].endswith("</div>")
-#
-#     def split_css(self):
-#         self.css_list = []
-#         start = None
-#         for i in range(len(self.lines)):
-#             line = self.lines[i].strip()
-#             if line.endswith("{") and line.startswith("."):
-#                 start = i
-#             elif line.endswith("}") and start is not None:
-#                 end = i
-#                 raw_css = "\n".join(self.lines[start:end + 1])
-#                 self.css_list.append({'raw': raw_css})
-#                 start = None
-#
-#     def simple_css(self):
-#         for i in range(len(self.css_list)):
-#             raw_css = self.css_list[i]['raw']
-#             self.css_list[i]['class_name'] = re.sub(r'{[\s\S]*}', '', raw_css).strip()
-#
-#             simple_css = raw_css
-#             simple_css = re.sub(r'.*\{', '{', simple_css)
-#             simple_css = re.sub(r'\n\s+background-image: url\((.*?)\).*\n', '\n', simple_css)
-#             self.css_list[i]['simple'] = simple_css
-#
-#     def final_matching(self):
-#         n=len(self.css_list)
-#         self.matching_pair_advance =[]
-#         matching_pairs = []
-#         for i in range(n):
-#             for j in range(i+1,n):
-#                 if self.css_list[i]["simple"]== self.css_list[j]["simple"]:
-#                     self.css_list.remove(self.css_list[j]["simple"])
-#                     matching_pairs.append(self.css_list[i]["simple"])
-#         self.matching_pairs = matching_pairs
-#     # def find_matching_pairs(self):
-#     #     n = len(self.css_list)
-#     #     self.matching_pair_advance = []
-#     #     for i in range(n):
-#     #         for j in range(i + 1, n):
-#     #             if self.css_list[i]['simple'] == self.css_list[j]['simple']:
-#     #                 self.insert_matching_pairs(self.css_list[i]['raw'], self.css_list[j]['raw'])
-#
-#     # def insert_matching_pairs(self, text_1, text_2):
-#     #     # matching_pairs = [[0 for j in range(len(text_1))] for i in range(text_2)]
-#     #     # matching_pairs = []
-#     #     # for h in range(len(self.css_list)):
-#     #     #     for k in range(h+1,len(self.css_list)):
-#     #     #         if self.css_list[h]['raw'] not in matching_pairs:
-#     #     #             text_1=matching_pairs.append(self.css_list[h]['raw'])
-#     #     #         if self.css_list[k]['raw'] not in matching_pairs:
-#     #     #             text_2=matching_pairs.append(self.css_list[k]['raw'])
-#     #     #
-#     #     # for h in range(len(self.matching_pairs)):
-#     #     #     m = self.matching_pairs[h]
-#     #     #     if
-#     #     #
-#     #     # self.matching_pairs = matching_pairs
-#     #     pass
-#
-#
-# def main():
-#     data = read_data("data.css")
-#     lines = data.split("\n")
-#
-#     split_css = SplitDiv()
-#     split_css.set_lines(lines)
-#     split_css.split_css()
-#     split_css.simple_css()
-#     split_css.final_matching()
-#     # for r in result:
-#     #     print(r)
-#     print("==============================")
-#     # for i in split_css.results:
-#     #     print(i)
-#     for css in split_css.matching_pairs:
-#         print(css)
-#
-#
-# if __name__ == "__main__":
-# #     main()
 import re
 
 def read_data(filepath):
@@ -190,7 +71,6 @@ class SplitDiv:
                 if self.css_list[i]["simple"] not in matching_pairs:
                     matching_pairs.append(self.css_list[i]["raw"])
                     matching_pairs.append("class_name" + "_" + self.css_list[i]['class_name'] + "\n\n")
-
         self.matching_pairs = matching_pairs
 
     def create_new_css_file(self):
@@ -212,7 +92,6 @@ def main():
     split_css.split_css()
     split_css.simple_css()
     # split_css.final_matching()
-
     # print("==============================")
     # for css in split_css.matching_pairs:
     #     print(css)
--- split_div.py
+++ split_div.py
@@ -1,4 +1,7 @@
 # import re
+# from difflib import SequenceMatcher
+#
+#
 # def read_data(filepath):
 #     with open(filepath, encoding="utf-8") as h:
 #         return h.read()
@@ -9,8 +12,9 @@
 #         self.lines = None
 #         self.space_length = None
 #         self.start_div = None
-#         self.result =None
-#         self.clear_div= None
+#         self.div_list = None
+#         self.modified_divs = None
+#         self.matching_pair_advance = None
 #
 #     def set_lines(self, lines):
 #         self.lines = lines
@@ -36,33 +40,49 @@
 #         return self.space_length[start] == self.space_length[now] and self.lines[now].endswith("</div>")
 #
 #     def split_div(self):
-#         result = []
-#         for i in range(1,len(self.lines)):
+#         self.div_list = []
+#         for i in range(1, len(self.lines)):
 #             if self.start_div[i]:
-#                 for j in range(i,len(self.lines)):
+#                 for j in range(i, len(self.lines)):
 #                     if self.space_length[i] == self.space_length[j] and self.lines[j].strip().endswith("</div>"):
-#
-#                         result.append({
+#                         text = '\n'.join(self.lines[i: j + 1])
+#                         result_item = {
 #                             'start': i,
 #                             'end': j,
-#                             'text': '\n'.join(self.lines[i: j+1]),
-#                             'simple_div' : self.clear_div
-#                         })
+#                             'text': text,
+#                             'simple': self.fun_clear_div(text)
+#                         }
+#                         self.div_list.append(result_item)
 #                         break
-#         print(f'\n\n{"#" * 100}\n\n')
-#         for t in result:
-#             print(t['text'])
-#             print(f'\n\n{"#"*100}\n\n')
-#         return result
+#         return self.div_list
 #
-#     def fun_clear_div(self, text):
-#         for i in range(len(text)):
-#             raw_div = text[i]
-#             clear_div = raw_div
-#             clear_div = re.sub(r'src="([^"]*)"', '" "', clear_div)
-#             clear_div = re.sub(r'<div[^>]*>(.*?)<\/div>', '\n', clear_div)
-#             text[i] = clear_div
-#         return self.clear_div
+#     def fun_clear_div(self, raw_div):
+#         clear_div = raw_div
+#         clear_div = re.sub(r'src="(.*)"', 'src=""', clear_div)
+#         clear_div = re.sub(r'>.*?</div>', '></div>', clear_div)
+#         return clear_div
+#
+#     def find_matching_component(self , modified_divs):
+#         n = len(modified_divs)
+#         matching_pair_advance = []
+#         raw_div= self.modified_divs["text"]
+#         for i in range(n):
+#             for j in range(i + 1, n):
+#                 seq_matcher = SequenceMatcher(None, self.modified_divs[i]['simple'], self.modified_divs[j]['simple'])
+#                 similarity_ratio = seq_matcher.ratio()
+#                 if similarity_ratio == 1:
+#                     matching_pair_advance.append((self.modified_divs[i]['text'], self.modified_divs[j]['text']))
+#         return matching_pair_advance
+#
+#     def new_div(self):
+#         compare = []
+#         new_css = ''
+#         for css in self.modified_divs:
+#             if css['text'] not in new_css:
+#                 new_css += css['class_name'] + ' ' + css['clear_div'] + '\n\n'
+#             else:
+#                 compare.append(css)
+#         return new_css, compare
 #
 #
 # def main():
@@ -71,10 +91,19 @@
 #     split_div = SplitDiv()
 #     split_div.set_lines(lines)
 #     split_div.split_div()
-#     split_div.fun_clear_div()
+#     # split_div.find_matching_component()
+#     for i in split_div.div_list:
+#         print('\n\n', '#'*100)
+#         print(i['simple'])
+#     # modified_divs = split_div.fun_clear_div()
+#     # split_div.new_div()
+#     for matching_pair in split_div.matching_pair_advance:
+#         print('\n\n','#'*100)
+#         print(matching_pair)
 #
 # if __name__ == "__main__":
 #     main()
+
 
 import re
 from difflib import SequenceMatcher
@@ -90,7 +119,7 @@ class SplitDiv:
         self.lines = None
         self.space_length = None
         self.start_div = None
-        self.result = None
+        self.div_list = None
         self.modified_divs = None
         self.matching_pair_advance = None
 
@@ -98,6 +127,7 @@ class SplitDiv:
         self.lines = lines
         self.count_space()
         self.calculate_start_div()
+        self.modified_divs = []  # Initialize modified_divs
 
     def count_space(self):
         cs = {}
@@ -118,7 +148,7 @@ class SplitDiv:
         return self.space_length[start] == self.space_length[now] and self.lines[now].endswith("</div>")
 
     def split_div(self):
-        self.result = []
+        self.div_list = []
         for i in range(1, len(self.lines)):
             if self.start_div[i]:
                 for j in range(i, len(self.lines)):
@@ -130,42 +160,30 @@ class SplitDiv:
                             'text': text,
                             'simple': self.fun_clear_div(text)
                         }
-                        self.result.append(result_item)
+                        self.div_list.append(result_item)
                         break
-
-        # print(f'\n\n{"#" * 100}\n\n')
-        # for t in self.result:
-        #     print(t['text'])
-        #     print(f'\n\n{"#"*100}\n\n')
-        return self.result
+        return self.div_list
 
     def fun_clear_div(self, raw_div):
         clear_div = raw_div
+        clear_div = re.sub(r'^\s*', '', clear_div)
+        clear_div = re.sub(r'\n\s*', '\n', clear_div)
         clear_div = re.sub(r'src="(.*)"', 'src=""', clear_div)
         clear_div = re.sub(r'>.*?</div>', '></div>', clear_div)
         return clear_div
 
-    def find_matching_component(self , modified_divs):
-        n = len(modified_divs)
+    def find_matching_component(self):
+        n = len(self.div_list)
         matching_pair_advance = []
-        raw_div= self.modified_divs["text"]
         for i in range(n):
             for j in range(i + 1, n):
-                seq_matcher = SequenceMatcher(None, self.modified_divs[i]['clear_div'], self.modified_divs[j]['clear_div'])
+                seq_matcher = SequenceMatcher(None, self.div_list[i]['simple'], self.div_list[j]['simple'])
                 similarity_ratio = seq_matcher.ratio()
-                if similarity_ratio >= 0.98:
-                    matching_pair_advance.append((self.modified_divs[i]['text'], self.modified_divs[j]['text']))
+                if similarity_ratio >= 0.95:
+                    if self.div_list[i]["simple"] not in matching_pair_advance:
+                        matching_pair_advance.append(self.div_list[i]['simple'])
+        self.matching_pair_advance = matching_pair_advance
         return matching_pair_advance
-
-    def new_div(self):
-        compare = []
-        new_css = ''
-        for css in self.modified_divs:
-            if css['text'] not in new_css:
-                new_css += css['class_name'] + ' ' + css['clear_div'] + '\n\n'
-            else:
-                compare.append(css)
-        return new_css, compare
 
 
 def main():
@@ -174,13 +192,14 @@ def main():
     split_div = SplitDiv()
     split_div.set_lines(lines)
     split_div.split_div()
-    # split_div.find_matching_component()
-    for matching_pair in split_div.result:
-        print('\n\n', '#'*100)
-        print(matching_pair['simple'])
-    # modified_divs = split_div.fun_clear_div()
-    # split_div.new_div()
+    split_div.find_matching_component()
+    # for i in split_div.div_list:
+    #     print('\n\n', '#'*100)
+    #     print(i['simple'])
+    for matching_pair in split_div.matching_pair_advance:
+        print('#' * 100)
+        print(matching_pair)
+
 
 if __name__ == "__main__":
     main()
-
